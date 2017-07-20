63a1282392413b36e51e5218223e298f8907f4ee
Shawn Lin <xiaoliang.lin@columbia.edu>
2017-07-20 02:01:29 -0400

    using quandl to pull data, some data is missing

--- R/create_return_series.R
+++ R/create_return_series.R
@@ -5,6 +5,9 @@ create_return_series <- function() {
   #' instead of the current NAV.
   #'
   #' @import dplyr
+  #' @importFrom Quandl Quandl
+  #' @importFrom Quandl Quandl.api_key
+  #'
   #' @export
 
   ## Load log file
@@ -14,14 +17,45 @@ create_return_series <- function() {
 
   log_file <-
     log_file %>%
-    arrange(Date)
+    arrange(Date) %>%
     group_by(Ticker) %>%
     mutate(InitiateInvestment = (cumsum(Quantity) - Quantity == 0))
 
-  initiation_list <-
+  price_get <-
     log_file %>%
     filter(InitiateInvestment) %>%
     select(Date, Ticker)
+
+  Quandl.api_key(PKG_OPTIONS()$quandlapi)
+  dir_price_data <- paste0(PKG_OPTIONS()$wd, "price_data/")
+  for (stock in 1:nrow(price_get)) {
+    ## File location of the price data for the ticker
+    ticker <- price_get[stock,]$Ticker
+    dir_price_file <- paste0(dir_price_data, "price_",ticker, ".csv")
+
+    if (!file.exists(dir_price_file)) {
+      ## if the price file does not exists, create one that contains all the
+      ## price information since position is initiated
+      start_date <- price_get[stock,]$Date
+      price_data <-
+        Quandl(paste0("WIKI/",ticker),
+               start_date = start_date,
+               end_date = "2017-07-15")
+      price_data <- arrange(price_data, Date)
+      write.csv(price_data, dir_price_file, row.names = FALSE)
+    } else {
+      price_data <- read.csv(dir_price_file)
+      start_date <- zoo::as.Date(tail(price_data$Date,1)) + lubridate::days(1)
+      new_price_data <-
+        Quandl(paste0("WIKI/",ticker),
+               start_date = start_date,
+               end_date = Sys.Date())
+      new_price_data <- arrange(new_price_data, Date)
+      names(price_data) <- names(new_price_data)
+      price_data <- rbind(price_data, new_price_data)
+    }
+
+  }
 
 
 }
--- R/set_pkg_options.R
+++ R/set_pkg_options.R
@@ -1,17 +1,20 @@
 # Variable, global to package's namespace.
 # This function is not exported to user space and does not need to be documented.
-PKG_OPTIONS <- settings::options_manager(wd = "C:/")
+PKG_OPTIONS <- settings::options_manager(wd = "C:/",
+                                         quandlapi = "17zLYKUTzzt6whHxFZEF")
 
 # User function that gets exported:
 set_pkg_options <- function(...) {
   #' Set or get options for my package
   #'
-  #' @param ... Option names to retrieve option values or \code{[key]=[value]} pairs to set options.
+  #' @param ... Option names to retrieve option values or
+  #'   \code{[key]=[value]} pairs to set options.
   #'
   #' @section Supported options:
   #' The following options are supported
   #' \itemize{
-  #'  \item{\code{wd}}{(\code{string};1) The working directory of all data. }
+  #'  \item{\code{wd}}{(\code{string}) The working directory of all data. }
+  #'  \item{\code{quandlapi}}{(\code{string}) Your API Key for Quandl. }
   #' }
   #' @export
 
